5dcbdb209b1f4e611a39c50ce7dc679a9ba8423f
Fandi <31383432+fandie@users.noreply.github.com>
2023-12-11 22:33:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Portofolio</title>
-    <meta http-equiv="X-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-
-    <!-- My Own Styles -->
-    <link rel="stylesheet" href="style.css" />
-
-    <!-- Google Fonts & icons -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  </head>
-
-  <body>
-    <header class="header">
-      <a href="#" class="logo">Portfolio.</a>
-
-      <input type="checkbox" id="check">
-      <label for="check" class="icons">
-        <i class='bx bx-menu' id="menu-icon"></i>
-        <i class='bx bx-x' id="close-icon"></i>
-      </label>
-
-      <nav class="navbar">
-        <a href="#home" class="active">Home</a>
-        <a href="#skill">Skill</a>
-        <a href="#experience">Experience Me</a>
-        <a href="#project">My Project</a>
-        <a href="contact.html">Contact</a>
-      </nav>
-    </header>
-
-
-<!-- Section Home -->
-<main>
-    <section id=home class="home">
-      <div class="home-content">
-        <h1>Hi, Saya Fandi Prasetyo</h1>
-        <span class="multiple-text"></span>
-        <p>Selamat datang di website saya, Saya senang sekali berbagi kisah dan pencapaian proyek-proyek kreatif saya. Temukan lebih banyak informasi tentang saya <a href="#about">disini</a></p>
-        <a href="https://drive.google.com/file/d/1DLgXQ-FAG5C2oVZ5ijWKIQNxtHfSfSzQ/view?usp=sharing" class="btn" target="_blank">Download CV</a>
-        <div class="socmed">
-          <a href="#"><i class='bx bxl-linkedin' ></i></a>
-          <a href="#"><i class='bx bxl-github'></i></a>
-          <a href="#"><i class='bx bxl-telegram' ></i></a>
-        </div>
-    </section>
-    <aside>
-      <img class="image" src="assets/image/index/astronaut_riding_rocket.png" alt="astronout">
-    </aside>
-  </main>
-
-
-<!-- Section About Me -->
-
-    <footer class="footer">
-      <p>&copy; by Fandi Prasetyo 2023.</p>
-    </footer>
-
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
-    <script src="index.js"></script>
-
-  </body>
-</html>
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Data Scientist", "Social Media Specialist"],
+  typeSpeed: 70,
+  backSpeed: 30,
+  backDelay: 500,
+  loop: true,
+});
