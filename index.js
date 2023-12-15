45e94e4b600a6563a652062ebb7767570cb4d371
Fandi <31383432+fandie@users.noreply.github.com>
2023-12-15 18:39:37 +0700

    Update index.js
    nav tab interaction

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ var typed = new Typed(".multiple-text", {
   backDelay: 500,
   loop: true,
 });
+
+ // Perubahan: Menambahkan fungsi untuk menangani perubahan warna pada tab
+ function updateTabColors(activeTabButton) {
+  // Mengambil semua tab button
+  var tabButtons = document.querySelectorAll('.tab-button');
+  // Menghapus class 'active' dari semua tab button
+  tabButtons.forEach(function (button) {
+    button.classList.remove('active');
+  });
+  // Menambah class 'active' pada tab button yang sedang aktif
+  activeTabButton.classList.add('active');
+}
+
+// Perubahan: Menyederhanakan fungsi showTab
+function showTab(tabId) {
+  // Mengambil semua tab pane
+  var tabs = document.querySelectorAll('.tab-pane');
+  // Menghapus class 'active' dari semua tab pane
+  tabs.forEach(function (tab) {
+    tab.classList.remove('active');
+  });
+
+  // Mengambil elemen tab pane yang sedang aktif
+  var selectedTab = document.getElementById(tabId);
+  // Menambah class 'active' pada tab pane yang sedang aktif
+  if (selectedTab) {
+    selectedTab.classList.add('active');
+  }
+
+  // Memanggil fungsi updateTabColors dengan tab button yang sedang aktif
+  updateTabColors(document.getElementById(tabId.replace('-pane', '')));
+}
